73f1e707af815429f6f5462f522be0fd3ff2a7b6
Anki139 <chauhan123anki@gmail.com>
2025-08-22 10:12:20 +0530

    er

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
-import imagekit from '../configs/imageKit';
-import Message from '../Models/Message';
+import imagekit from '../configs/imageKit.js';
+import Message from '../Models/Message.js';
 const connections={}
  
 export const sseController=(req , res)=>{
